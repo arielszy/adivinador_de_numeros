79701333e72fcdc9413d055f0f33f034c251d2d7
ariel <arielszy@yahoo.com.ar>
2021-10-03 22:27:52 -0300

    set style of buttons

--- lib/Screens/game.dart
+++ lib/Screens/game.dart
@@ -1,5 +1,4 @@
 import 'package:adivinador_de_numeros/Widgets/backgr.dart';
-import 'package:adivinador_de_numeros/Widgets/btn.dart';
 import 'package:adivinador_de_numeros/Widgets/cardWidget.dart';
 import 'package:adivinador_de_numeros/resources/cards_values.dart';
 import 'package:flutter/material.dart';
@@ -20,15 +19,37 @@ class Game extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Image.asset(
-                    'y.png',
-                    width: 75,
-                    height: 75,
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      elevation: 0,
+                      onPrimary: Colors.transparent,
+                      onSurface: Colors.transparent,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(100))),
+                    ),
+                    onPressed: () {},
+                    child: Image.asset(
+                      'y.png',
+                      width: 75,
+                      height: 75,
+                    ),
                   ),
-                  Image.asset(
-                    'n.png',
-                    width: 75,
-                    height: 75,
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      elevation: 0,
+                      onPrimary: Colors.transparent,
+                      onSurface: Colors.transparent,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(100))),
+                    ),
+                    onPressed: () {},
+                    child: Image.asset(
+                      'n.png',
+                      width: 75,
+                      height: 75,
+                    ),
                   ),
                 ],
               )
